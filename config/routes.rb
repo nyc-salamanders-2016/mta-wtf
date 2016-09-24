154cb2ae635c39d5fcb947c80b5cc5db4122fd30
Joshua Marantz <marantzj@gmail.com>
2016-09-24 18:58:33 -0400

    Update routes.rb

--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,5 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   get '/', to: 'pages#index'
   get '/latest', to: 'pages#latest'
+  get '/stations', to: 'pages#all_stations'
 end
