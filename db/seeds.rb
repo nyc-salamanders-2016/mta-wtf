4e78cbb61c64c10a4e1f6dd98f83884f40d07c2b
JNaftali <marantzj@gmail.com>
2016-09-24 14:13:12 -0400

    attempting to seed with csv

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,11 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+require 'csv'
+
+path = Rails.root.join('lib', 'seeds', 'stations.csv')
+
+CSV.foreach(path, {headers: true, converters: :numeric, header_converters: :symbol}) do |row|
+  binding.pry
+  line = Line.find_or_create_by(name: row[:line].to_s)
+end
