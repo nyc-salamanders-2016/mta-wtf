15f219bb98c28ba3ca29efcc04d9b52633904010
JNaftali <marantzj@gmail.com>
2016-09-23 20:57:11 -0400

    WOOHOO DREW THE 1 TRAIN FROM A CSV AND ADDED A MOUSEOVER EVENT MUTHAAAASSSSS

--- src/App.js
+++ src/App.js
@@ -1,63 +1,47 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-// import Map, {InfoWindow, Marker} from 'google-maps-react'
-import GMap from './GMap'
-import './App.css';
+import React, { Component } from 'react'
+import logo from './logo.svg'
+import './App.css'
+import stops from './stops'
 
+const google = window.google
 
 class App extends Component {
   componentDidMount() {
-    this.map = new window.google.maps.Map(this.refs.map, {
+    this.map = new google.maps.Map(this.refs.map, {
       center: {lat: 40.7048981, lng: -74.012385},
       zoom: 14
     })
 
-    // this.map.data.loadGeoJson('data.js')
-
-    const lines = [
-      [
-        {lat: 40.706, lng: -74.009},
-        {lat: 40.702, lng: -74.009}
-      ],
-      [
-        {lat: 40.704, lng: -74.009},
-        {lat: 40.704, lng: -74.011}
-      ],
-      [
-        {lat: 40.702, lng: -74.011},
-        {lat: 40.706, lng: -74.011}
-      ]
-    ]
+    const lines = this.getLines(stops)
 
     lines.forEach((coord_list) => this.drawLine(coord_list))
   }
 
-  renderFlights(){
-    var flightPlanCoordinates = [
-    {lat: 40.706, lng: -74.009},
-    {lat: 40.703, lng: -74.009}
-    ];
+  connectLatLng(stopa, stopb) {
+    return [{ lat: stopa.stop_lat, lng: stopa.stop_lon },
+    { lat: stopb.stop_lat, lng: stopb.stop_lon }]
+  }
 
-    var flightPath = new window.google.maps.Polyline({
-    path: flightPlanCoordinates,
-    geodesic: true,
-    strokeColor: '#FF0000',
-    strokeOpacity: 1.0,
-    strokeWeight: 2
-    })
-
-    flightPath.setMap(this.map)
+  getLines(stops) {
+    return stops.filter((stop) => stop.line === 1)
+         .reduce(
+           (newArray, stop, index, array) => {
+             if (!(index >= array.length - 2)) {
+               newArray.push(this.connectLatLng(stop, array[index+1]))
+             }
+             return newArray
+         }, [] )
   }
 
   drawLine(coord_list) {
-    console.log(coord_list)
-    const path = new window.google.maps.Polyline({
+    const path = new google.maps.Polyline({
       path: coord_list,
       geodesic: true,
       strokeColor: '#FF0000',
       strokeOpacity: 1.0,
       strokeWeight: 2
     })
+    google.maps.event.addListener(path, "mouseover", () => console.log("I'm the 1 train!"))
     path.setMap(this.map)
   }
 
---
+++ src/stops.js
@@ -0,0 +1,308 @@
+let stops = [
+	{
+		"stop_id": 101,
+		"stop_name": "Van Cortlandt Park - 242 St",
+		"stop_lat": 40.889248,
+		"stop_lon": -73.898583,
+		"line": 1,
+		"order": 1
+	},
+	{
+		"stop_id": 103,
+		"stop_name": "238 St",
+		"stop_lat": 40.884667,
+		"stop_lon": -73.90087,
+		"line": 1,
+		"order": 2
+	},
+	{
+		"stop_id": 104,
+		"stop_name": "231 St",
+		"stop_lat": 40.878856,
+		"stop_lon": -73.904834,
+		"line": 1,
+		"order": 3
+	},
+	{
+		"stop_id": 106,
+		"stop_name": "Marble Hill - 225 St",
+		"stop_lat": 40.874561,
+		"stop_lon": -73.909831,
+		"line": 1,
+		"order": 4
+	},
+	{
+		"stop_id": 107,
+		"stop_name": "215 St",
+		"stop_lat": 40.869444,
+		"stop_lon": -73.915279,
+		"line": 1,
+		"order": 5
+	},
+	{
+		"stop_id": 108,
+		"stop_name": "207 St",
+		"stop_lat": 40.864621,
+		"stop_lon": -73.918822,
+		"line": 1,
+		"order": 6
+	},
+	{
+		"stop_id": 109,
+		"stop_name": "Dyckman St",
+		"stop_lat": 40.860531,
+		"stop_lon": -73.925536,
+		"line": 1,
+		"order": 7
+	},
+	{
+		"stop_id": 110,
+		"stop_name": "191 St",
+		"stop_lat": 40.855225,
+		"stop_lon": -73.929412,
+		"line": 1,
+		"order": 8
+	},
+	{
+		"stop_id": 111,
+		"stop_name": "181 St",
+		"stop_lat": 40.849505,
+		"stop_lon": -73.933596,
+		"line": 1,
+		"order": 9
+	},
+	{
+		"stop_id": 112,
+		"stop_name": "168 St - Washington Hts",
+		"stop_lat": 40.840556,
+		"stop_lon": -73.940133,
+		"line": 1,
+		"order": 10
+	},
+	{
+		"stop_id": 113,
+		"stop_name": "157 St",
+		"stop_lat": 40.834041,
+		"stop_lon": -73.94489,
+		"line": 1,
+		"order": 11
+	},
+	{
+		"stop_id": 114,
+		"stop_name": "145 St",
+		"stop_lat": 40.826551,
+		"stop_lon": -73.95036,
+		"line": 1,
+		"order": 12
+	},
+	{
+		"stop_id": 115,
+		"stop_name": "137 St - City College",
+		"stop_lat": 40.822008,
+		"stop_lon": -73.953676,
+		"line": 1,
+		"order": 13
+	},
+	{
+		"stop_id": 116,
+		"stop_name": "125 St",
+		"stop_lat": 40.815581,
+		"stop_lon": -73.958372,
+		"line": 1,
+		"order": 14
+	},
+	{
+		"stop_id": 117,
+		"stop_name": "116 St - Columbia University",
+		"stop_lat": 40.807722,
+		"stop_lon": -73.96411,
+		"line": 1,
+		"order": 15
+	},
+	{
+		"stop_id": 118,
+		"stop_name": "Cathedral Pkwy",
+		"stop_lat": 40.803967,
+		"stop_lon": -73.966847,
+		"line": 1,
+		"order": 16
+	},
+	{
+		"stop_id": 119,
+		"stop_name": "103 St",
+		"stop_lat": 40.799446,
+		"stop_lon": -73.968379,
+		"line": 1,
+		"order": 17
+	},
+	{
+		"stop_id": 120,
+		"stop_name": "96 St",
+		"stop_lat": 40.793919,
+		"stop_lon": -73.972323,
+		"line": 1,
+		"order": 18
+	},
+	{
+		"stop_id": 121,
+		"stop_name": "86 St",
+		"stop_lat": 40.788644,
+		"stop_lon": -73.976218,
+		"line": 1,
+		"order": 19
+	},
+	{
+		"stop_id": 122,
+		"stop_name": "79 St",
+		"stop_lat": 40.783934,
+		"stop_lon": -73.979917,
+		"line": 1,
+		"order": 20
+	},
+	{
+		"stop_id": 123,
+		"stop_name": "72 St",
+		"stop_lat": 40.778453,
+		"stop_lon": -73.98197,
+		"line": 1,
+		"order": 21
+	},
+	{
+		"stop_id": 124,
+		"stop_name": "66 St - Lincoln Center",
+		"stop_lat": 40.77344,
+		"stop_lon": -73.982209,
+		"line": 1,
+		"order": 22
+	},
+	{
+		"stop_id": 125,
+		"stop_name": "59 St - Columbus Circle",
+		"stop_lat": 40.768247,
+		"stop_lon": -73.981929,
+		"line": 1,
+		"order": 23
+	},
+	{
+		"stop_id": 126,
+		"stop_name": "50 St",
+		"stop_lat": 40.761728,
+		"stop_lon": -73.983849,
+		"line": 1,
+		"order": 24
+	},
+	{
+		"stop_id": 127,
+		"stop_name": "Times Sq - 42 St",
+		"stop_lat": 40.75529,
+		"stop_lon": -73.987495,
+		"line": 1,
+		"order": 25
+	},
+	{
+		"stop_id": 128,
+		"stop_name": "34 St - Penn Station",
+		"stop_lat": 40.750373,
+		"stop_lon": -73.991057,
+		"line": 1,
+		"order": 26
+	},
+	{
+		"stop_id": 129,
+		"stop_name": "28 St",
+		"stop_lat": 40.747215,
+		"stop_lon": -73.993365,
+		"line": 1,
+		"order": 27
+	},
+	{
+		"stop_id": 130,
+		"stop_name": "23 St",
+		"stop_lat": 40.744081,
+		"stop_lon": -73.995657,
+		"line": 1,
+		"order": 28
+	},
+	{
+		"stop_id": 131,
+		"stop_name": "18 St",
+		"stop_lat": 40.74104,
+		"stop_lon": -73.997871,
+		"line": 1,
+		"order": 29
+	},
+	{
+		"stop_id": 132,
+		"stop_name": "14 St",
+		"stop_lat": 40.737826,
+		"stop_lon": -74.000201,
+		"line": 1,
+		"order": 30
+	},
+	{
+		"stop_id": 133,
+		"stop_name": "Christopher St - Sheridan Sq",
+		"stop_lat": 40.733422,
+		"stop_lon": -74.002906,
+		"line": 1,
+		"order": 31
+	},
+	{
+		"stop_id": 134,
+		"stop_name": "Houston St",
+		"stop_lat": 40.728251,
+		"stop_lon": -74.005367,
+		"line": 1,
+		"order": 32
+	},
+	{
+		"stop_id": 135,
+		"stop_name": "Canal St",
+		"stop_lat": 40.722854,
+		"stop_lon": -74.006277,
+		"line": 1,
+		"order": 33
+	},
+	{
+		"stop_id": 136,
+		"stop_name": "Franklin St",
+		"stop_lat": 40.719318,
+		"stop_lon": -74.006886,
+		"line": 1,
+		"order": 34
+	},
+	{
+		"stop_id": 137,
+		"stop_name": "Chambers St",
+		"stop_lat": 40.715478,
+		"stop_lon": -74.009266,
+		"line": 1,
+		"order": 35
+	},
+	{
+		"stop_id": 138,
+		"stop_name": "Cortlandt St",
+		"stop_lat": 40.711835,
+		"stop_lon": -74.012188,
+		"line": 1,
+		"order": 36
+	},
+	{
+		"stop_id": 139,
+		"stop_name": "Rector St",
+		"stop_lat": 40.707513,
+		"stop_lon": -74.013783,
+		"line": 1,
+		"order": 37
+	},
+	{
+		"stop_id": 140,
+		"stop_name": "South Ferry Loop",
+		"stop_lat": 40.701411,
+		"stop_lon": -74.013205,
+		"line": 1,
+		"order": 38
+	}
+]
+
+export default stops
