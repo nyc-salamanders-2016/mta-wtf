e51a0fded28fe15c8a7b46449db41e104c21c304
JNaftali <marantzj@gmail.com>
2016-09-24 17:38:22 -0400

    refactored for clarity, and to iterate through all lines

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -5,21 +5,39 @@ class PagesController < ApplicationController
   def latest
     xml = Net::HTTP.get(URI('http://web.mta.info/status/serviceStatus.txt'))
     hash = Hash.from_xml(xml)
-    html = Nokogiri::HTML(hash['service']['subway']['line'][0]['text'])
-    cancelations = html.css('b').map(&:inner_text).select {|string| string.match(/\[(.)\] No trains/) }
-    parsed_info = cancelations.map {|text| text.match(/\[(.)\] No trains (.*)/) }
-    result = []
-    parsed_info.each do |match|
-      hash = {}
-      hash[:line] = match[1]
-      case match[2]
-      when /running/
-        hash[:canceled] = true
+    html = hash['service']['subway']['line'].reduce('') do |string, hash|
+      if hash['text']
+        string + hash['text']
       else
-        hash[:canceled] = match[2].match(/between (.*) and (.*)/)[1,2]
+        string
       end
-      result << hash
     end
-    render json: result
+    nok = Nokogiri::HTML(html)
+    info = parse_all_live_data(nok)
+    render json: info
+  end
+
+  private
+  def parse_all_live_data(nok)
+    result = []
+    result += find_all_cancelations(nok)
+  end
+
+  def find_cancelations(nok)
+    strings = nok.css('b').map(&:inner_text).select {|string| string.match(/\[(.)\] No trains/) }
+    matches = strings.map {|text| text.match(/\[(.)\] No trains (.*)/) }
+    matches.map {|match| parse_value(match)}
+  end
+
+  def parse_value(match)
+    hash = {}
+    hash[:line] = match[1]
+    case match[2]
+    when /running/
+      hash[:canceled] = true
+    else
+      hash[:canceled] = match[2].match(/between (.*) and (.*)/)[1,2]
+    end
+    result << hash
   end
 end
