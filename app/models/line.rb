914c51e5b408e6184592407f63f68cc3f353db3f
JNaftali <marantzj@gmail.com>
2016-09-26 14:11:19 -0400

    name of branch

--- app/models/line.rb
+++ app/models/line.rb
@@ -1,4 +1,13 @@
 class Line < ApplicationRecord
   has_many :line_stations
   has_many :stations, through: :line_stations
+
+  def stations_that_connect_to(station)
+    stations = self.stations.sort_by {|sta| sta.order(self)}
+    i = stations.find_index{ |sta| station.id == sta.id }
+    result = []
+    result << stations[i-1] if stations[i-1]
+    result << stations[i+1] if stations[i+1]
+    result
+  end
 end
--- app/models/station.rb
+++ app/models/station.rb
@@ -1,4 +1,14 @@
 class Station < ApplicationRecord
   has_many :line_stations
   has_many :lines, through: :line_stations
+
+  def order(line)
+    LineStation.find_by(line: line, station: self).order
+  end
+
+  def connected_stations
+    self.lines.map do |line|
+      {line: line.name, stations: line.stations_that_connect_to(self)}
+    end
+  end
 end
