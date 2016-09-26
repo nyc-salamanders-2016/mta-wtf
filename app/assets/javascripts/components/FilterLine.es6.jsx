3477f60d34d8108c5df9dc441a590be689880c57
Henri El Daher <henrieldaher@gmail.com>
2016-09-26 18:59:54 -0400

    complete mvp of filtering lines

--- app/assets/javascripts/components/FilterLine.es6.jsx
+++ app/assets/javascripts/components/FilterLine.es6.jsx
@@ -1,4 +1,28 @@
 class FilterLine extends React.Component {
+  constructor() {
+    super()
+    this.toggleAllCheckboxes = this.toggleAllCheckboxes.bind(this)
+
+    this.gatherCheckboxes = this.gatherCheckboxes.bind(this)
+    this.checkboxes = []
+  }
+
+  toggleAllCheckboxes() {
+    this.props.lines.forEach((line) => {
+      this.props.toggleLineCheckbox(line.name, this.refs.selectbox.checked)
+    })
+
+    this.checkboxes.forEach((box) => {
+      box.checked = this.refs.selectbox.checked
+    })
+  }
+
+  gatherCheckboxes(box) {
+    this.checkboxes.push(box)
+  }
+
+
+
   render () {
     return(
       <div id="line-input-container">
@@ -6,13 +30,13 @@ class FilterLine extends React.Component {
       <h4> Solid Line: trains are running</h4>
       <h4> Dashed Line: trains are delayed</h4>
         <section>
-          <input ref="selectbox" type="checkbox" value="Select Line" /><span>Check/Uncheck all boxes</span>
+          <input onClick={this.toggleAllCheckboxes} ref="selectbox" type="checkbox" defaultChecked={true} /><span>Show/Hide all lines</span>
         </section>
 
         {
           this.props.lines.map((line,i) => {
             return (
-              <Line toggleLineCheckbox={this.props.toggleLineCheckbox} data={line} key={i} />
+              <Line gatherCheckboxes={this.gatherCheckboxes} toggleLineCheckbox={this.props.toggleLineCheckbox} data={line} key={i} />
             )
           })
         }
