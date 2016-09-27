4c29481ea56f851d634f58813dce93498f48e3d2
elisuh <elisaverna@gmail.com>
2016-09-27 18:56:54 -0400

    added what lines are currently stopping at each stop

--- app/assets/javascripts/components/StationDetails.es6.jsx
+++ app/assets/javascripts/components/StationDetails.es6.jsx
@@ -1,8 +1,22 @@
 class StationDetails extends React.Component {
+
+  getStationConnections(station) {
+    return station.line_stations.map((linestation) => {
+      return this.props.lines.find((line) => {
+        return line.id === linestation.line_id
+      })
+      })
+    };
+
   render(){
+    let lineNames = this.getStationConnections(this.props.showStation).map((line)=>{
+      return line.name
+    }).join(', ')
+
     return(
       <div>
-      <p>This is the {this.props.showStation} station.</p>
+      <p>This is the {this.props.showStation.name} station.</p>
+      <p>Lines currently serving this station: {lineNames}</p>
       </div>
     )
   }
