78357536275d25aa6e198a45a17fe21349392ff0
JNaftali <marantzj@gmail.com>
2016-09-28 17:32:48 -0400

    tweak

--- app/assets/javascripts/components/App.es6.jsx
+++ app/assets/javascripts/components/App.es6.jsx
@@ -10,9 +10,18 @@ class App extends React.Component {
     this.setInfoWindow = this.setInfoWindow.bind(this)
     this.toggleLineCheckbox = this.toggleLineCheckbox.bind(this)
     this.clickInfoWindow = this.clickInfoWindow.bind(this)
+    this.refreshPage = this.refreshPage.bind(this)
   }
 
   componentDidMount() {
+    refreshPage()
+
+    this.props.lines.forEach((line)=> {
+      this.state.lineToggles[line.name] = true
+    })
+  }
+
+  refreshPage() {
     $.ajax({
       url: '/latest',
       method: 'get'
@@ -20,10 +29,6 @@ class App extends React.Component {
       this.setState({
         liveStatus: response
       })
-    })
-
-    this.props.lines.forEach((line)=> {
-      this.state.lineToggles[line.name] = true
     })
   }
 
@@ -62,6 +67,12 @@ class App extends React.Component {
           <KeyWindow />
           <FilterLineWindow toggleLineCheckbox={this.toggleLineCheckbox} lines={this.props.lines} lineToggles={this.state.lineToggles} />
           <InfoWindow liveStatus={liveStatus} lines={this.props.lines} showLine={this.state.infoWindowLine} showStation={this.state.infoWindowStation} defaultLine={this.state.clickWindowLine} defaultStation={this.state.clickWindowStation} />
+          <div id="refresh-timestamp">
+            <form className="refresh-timestamp">
+              <button onClick={this.refreshPage} type="button">Reload</button>
+            </form>
+            <p>Updated: {Date()}</p>
+          </div>
         </div>
         <Map lineToggles={this.state.lineToggles} trackMouse={this.mouseCoords} liveStatus={liveStatus} lines={this.props.lines} stations={this.props.lines.reduce((ary,line) => {return ary.concat(line.stations)},[])} google={window.google} mapStyle={mapStyle}
              lineHover={this.setInfoWindow} stationHover={this.setInfoWindow} clickInfoWindow={this.clickInfoWindow} />
