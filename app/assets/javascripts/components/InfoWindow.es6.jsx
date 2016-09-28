854b11a7e155840686b01372cf145ae34caa58bd
JNaftali <marantzj@gmail.com>
2016-09-28 14:33:12 -0400

    Add delay info to line details box

--- app/assets/javascripts/components/InfoWindow.es6.jsx
+++ app/assets/javascripts/components/InfoWindow.es6.jsx
@@ -3,13 +3,13 @@ class InfoWindow extends React.Component {
   getActiveLine() {
     return this.props.lines.find((line) => line.name === this.props.showLine)
   }
-  
+
   render(){
     var line = this.props.lines.find((line) => line.name === this.props.showLine)
     return (
       <div className="panel panel-default" id="info-container" >
         <div className="panel-body">
-          { this.props.showStation ? <StationDetails showStation={this.props.showStation} lines={this.props.lines} /> :  this.props.showLine ? <LineDetails line={this.getActiveLine()} /> : <p>Please hover over a line or station for current service status.</p> }
+          { this.props.showStation ? <StationDetails showStation={this.props.showStation} lines={this.props.lines} /> :  this.props.showLine ? <LineDetails liveStatus={this.props.liveStatus} line={this.getActiveLine()} /> : <p>Please hover over a line or station for current service status.</p> }
         </div>
       </div>
     )
