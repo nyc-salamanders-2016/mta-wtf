55314a79f239c66924bf4701fd46db725a4f7118
JNaftali <marantzj@gmail.com>
2016-09-28 14:33:12 -0400

    Add delay info to line details box

--- app/assets/javascripts/components/LineDetails.es6.jsx
+++ app/assets/javascripts/components/LineDetails.es6.jsx
@@ -1,4 +1,13 @@
 class LineDetails extends React.Component{
+  isDuplicate(object, other_object) {
+    return Object.keys(object).all((key)=> object[key] === other_object[key])
+  }
+
+  relevantDelays() {
+    // const relevant = this.props.liveStatus.filter((update) => update.line === this.props.line.name)
+    const update = this.props.liveStatus.find((update)=> update.line === this.props.line.name && update.status === "delays")
+    return update ? `This line is experiencing delays going ${update.direction === "both" ? "both ways" : update.direction} because of ${update.reason} at ${update.reason_station}` : null
+  }
 
   stationNames() {
     return this.props.line.stations.map((station, i) => <li key={i}>{station.name}</li>)
@@ -7,7 +16,8 @@ class LineDetails extends React.Component{
   render(){
     return(
       <div>
-      <p>This is the {this.props.showLine} train.</p>
+      <p>This is the {this.props.line.name} train.</p>
+      <p>{this.relevantDelays()}</p>
       <ul>{this.stationNames()}</ul>
       </div>
     )
