ffec6990dbfb2de582532b2aa788ea4873981525
JNaftali <marantzj@gmail.com>
2016-09-28 13:36:21 -0400

    Replicated Henry's work from last night

--- app/assets/javascripts/components/LineDetails.es6.jsx
+++ app/assets/javascripts/components/LineDetails.es6.jsx
@@ -1,20 +1,14 @@
 class LineDetails extends React.Component{
 
-  stationNames(line) {
-    return line.stations.reduce((result, station, index, allStations) => {
-      return result + station.name + (index < allStations.length - 1 ? ', ' : ' and ')
-    }, '')
-  }
-
-  lineInfo(line) {
-    if (line) return `Right now, it stops at ${this.stationNames(line)}`
+  stationNames() {
+    return this.props.line.stations.map((station, i) => <li key={i}>{station.name}</li>)
   }
 
   render(){
     return(
       <div>
       <p>This is the {this.props.showLine} train.</p>
-      <p>{this.lineInfo(this.props.lines.find((line) => line.name === this.props.showLine))}</p>
+      <ul>{this.stationNames()}</ul>
       </div>
     )
   }
