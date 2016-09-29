23fb8e602764ce66a7b98565d32df8ce1174bd5f
Daniel Yoon <sangchu3102@gmail.com>
2016-09-29 09:16:49 -0400

    move image files

--- app/assets/javascripts/components/NavBar.es6.jsx
+++ app/assets/javascripts/components/NavBar.es6.jsx
@@ -2,7 +2,7 @@ class NavBar extends React.Component {
   render(){
     return(
         <nav id="header-css">
-          <img id="logo" src="logo8.png" />
+          <img id="logo" src="final_animated_GIF.gif" />
         </nav>
     )
   }
