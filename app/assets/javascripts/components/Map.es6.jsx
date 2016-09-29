b8e187ed8f72f86c4c0d1f37a8b9bfc80f824d13
Henri El Daher <henrieldaher@gmail.com>
2016-09-28 20:50:31 -0400

    get some DC stuff on the map

--- app/assets/javascripts/components/Map.es6.jsx
+++ app/assets/javascripts/components/Map.es6.jsx
@@ -3,30 +3,9 @@ class Map extends React.Component {
     super()
 
     this.lineColors = {
-      1: '#EE352E',
-      2: '#EE352E',
-      3: '#EE352E',
-      4: '#00933C',
-      5: '#00933C',
-      6: '#00933C',
-      '6E': '#00933C',
-      A: '#0039A6',
-      C: '#0039A6',
-      E: '#0039A6',
-      B: '#FF6319',
-      D: '#FF6319',
-      F: '#FF6319',
-      M: '#FF6319',
-      G: '#6CBE45',
-      J: '#996633',
-      Z: '#996633',
-      L: '#A7A9AC',
-      N: '#FCCC0A',
-      Q: '#FCCC0A',
-      R: '#FCCC0A',
-      S: '#808183',
-      7: '#B933AD',
-      '7E': '#B933AD'
+      'RD': '#EE352E',
+      'BL': '#EE352E',
+      'GR': '#B933AD'
     }
     this.lineObjects = {}
     this.mapStyles = [
