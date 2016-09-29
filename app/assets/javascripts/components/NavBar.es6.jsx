ac07a723172644d291ea7926f9284c84b438babf
JNaftali <marantzj@gmail.com>
2016-09-29 09:18:11 -0400

    animated logo

--- app/assets/javascripts/components/NavBar.es6.jsx
+++ app/assets/javascripts/components/NavBar.es6.jsx
@@ -2,7 +2,7 @@ class NavBar extends React.Component {
   render(){
     return(
         <nav style={{margin: '10px 15%'}} id="header-css">
-          <img className='img-responsive' id="logo" src="logo8.png" />
+          <img className='img-responsive' id="logo" src="final_animated_GIF.gif" />
         </nav>
     )
   }
